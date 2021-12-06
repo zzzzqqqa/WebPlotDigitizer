d6045d4ce699d77911225448901239e2e35b29c6
zzzzqqqa <zzzzqqq@gmail.com>
2021-12-06 19:07:11 +0800

    Update setupUbuntuDev.sh

--- setupUbuntuDev.sh
+++ setupUbuntuDev.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 # Setup an Ubuntu 20.04 system for development
+export HTTP_PROXY=http://proxy.eu.novartis.net:2010
+export http_proxy=http://proxy.eu.novartis.net:2010
+export PROXY_HTTP=http://proxy.eu.novartis.net:2010
 
 # install Ubuntu packages
 sudo -E apt install \
